65399bdea5ff1381d6673dba2c00d97e5650ed95
Ian Ibbotson <ianibbo@gmail.com>
2021-04-12 10:10:33 +0100

    change script name

--- useAlternateRepo.js
+++ useAlternateRepo.js
@@ -1,14 +1,19 @@
 const saveFile = require('fs').writeFileSync;
 
 const pkgJsonPath = require.main.paths[0].split('node_modules')[0] + 'package.json';
-
 const json = require(pkgJsonPath);
+const args = process.argv.slice(2);
 
-if (!json.hasOwnProperty('scripts')) {
-  json.scripts = {};
+console.log("Args: %o",args);
+if ( args.length == 1 ) {
+  console.log("Changing publishConfig.registry to %s",args[0]);
+  if (!json.hasOwnProperty('scripts')) {
+    json.scripts = {};
+  }
+  
+  // json.scripts['watch'] = '<some_commands_here>';
+  // json.publishConfig.registry="https://nexus.libsdev.k-int.com/repository/libsdev-npm-hosted/";
+  json.publishConfig.registry=args[0]
+  
+  saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
 }
-
-// json.scripts['watch'] = '<some_commands_here>';
-json.publishConfig.registry="https://nexus.libsdev.k-int.com/repository/libsdev-npm-hosted/";
-
-saveFile(pkgJsonPath, JSON.stringify(json, null, 2));
